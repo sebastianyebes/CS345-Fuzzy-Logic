525b102c08b08878e8b33d89311920b2a74cb40f
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2022-12-09 23:52:06 +0800

    Remove code smell for initialized files

--- YebesFuzzyLogic/YebesFuzzyLogic/Form1.cs
+++ YebesFuzzyLogic/YebesFuzzyLogic/Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using DotFuzzy;
+
 namespace YebesFuzzyLogic
 {
     public partial class Form1 : Form
